ddb4eb81c44819c667ee9d530947f1004227e7e6
Amos06 <chuangdayou@gmail.com>
2024-03-19 04:39:02 +0800

    vercel

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const app = express();
 const authClient = new auth.OAuth2User({
   client_id: process.env.CLIENT_ID as string,
   client_secret: process.env.CLIENT_SECRET as string,
-  callback: "http://127.0.0.1:3000/callback",
+  callback: "https://twitter-auth-nu.vercel.app/callback",
   scopes: ["tweet.read", "users.read"],
 });
 
